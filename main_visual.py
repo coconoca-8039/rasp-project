3fbdc421392d4ab305f52df52299df37808a9fa2
coconoca-8039 <coconoca.8039@gmail.com>
2024-02-14 22:22:07 +0900

    rasp

--- main_visual.py
+++ main_visual.py
@@ -1,25 +1,24 @@
 import os
 import posix_ipc
 import sys
-from can_all import can_get_0A1224AA
+import time
+import sched
+from can_0A1224AA import can_get_0A1224AA
 
 def handle_client(mq):
     while True:
         try:
             message, _ = mq.receive()
-            # print(type(message))  # bytes of class
             can_data = message.decode().split(' ')
             # print(type(can_data))
              
             # print(can_data)  # 受信したデータを全表示
-
+            
+            # ここの処理を６時間おきとかで実行する
             if can_data[2] == '0A1234AA':
                 tmp = can_get_0A1224AA(can_data, '0A1234AA')
                 print(tmp)
                 print("  ")
-                
-            #tmp = can_get_0A1224AA(can_data, '0A1234AA')
-            #print(tmp)
             
         except posix_ipc.BusyError:
             print("Message queue is full, skipping this message.")
